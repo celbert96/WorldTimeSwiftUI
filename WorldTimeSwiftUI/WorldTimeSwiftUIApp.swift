25d3a7c19741c5424cd644ca446a87935c61d417
Christopher Elbert <celbert96@gmail.com>
2022-08-15 22:25:44 -0400

    Initial Commit

---
+++ WorldTimeSwiftUI/WorldTimeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTimeSwiftUIApp.swift
+//  WorldTimeSwiftUI
+//
+//  Created by Christopher Elbert on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldTimeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
